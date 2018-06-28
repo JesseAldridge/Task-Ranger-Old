f44999e373726c3bf935264aa0f4fc15089b4277
Jesse Aldridge <jesse.aldridge@airbnb.com>
2018-06-28 11:50:11 -0700

    more nodes

--- weighted_choice.py
+++ weighted_choice.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import random
 
 def weighted_choice(choices):
@@ -29,8 +30,8 @@ def node(weight, name, children=None):
 def category(weight, name, sub_categories=None):
   return node(weight, name, children=[
     node(.1, 'wildcard'),
-    node(.15, 'categorize_tasks'),
-    node(.15, 'cleanup_notes'),
+    node(.1, 'categorize_tasks'),
+    node(.1, 'cleanup_notes'),
     node(.7, 'review_categorized', sub_categories or [])
   ])
 
@@ -38,48 +39,72 @@ def sub_categories(weights):
   return [node(weight, str(weight)) for weight in weights]
 
 if __name__ == '__main__':
-  categories = [
-    node(.3, 'startup', [
-      node(.5, 'market size', [
-        node(2300, 'apparel'),
-        node(1000, 'coding ai', [
+  categories = [ # 1 year
+    node(.3, 'startup', [  # 15 weeks
+      node(.8, 'work on existing ideas', [ # 12 weeks
+        node(157, 'coding ai', [
           node(.4, 'learn'),
           node(.3, 'random notes'),
           node(.1, 'build stuff'),
           node(.1, 'plan'),
           node(.1, 'use cases'),
         ]),
-        node(1000, 'consumer electronics'),
-        node(550, 'better ads'),
-        node(260, 'better linkedin'),
-        node(30, 'adult education'),
+        node(50, 'better linkedin'),
+        node(12, 'adult education'),
+        node(10, 'reputation system'),
+        node(9, 'better notes'),
+        node(8, 'better web browser'),
+        node(8, '10x vote'),
+        node(7, 'cardbrew.com'),
+        node(7, "better dr's appointments"),
+        node(7, "better meetup.com"),
+        node(7, "pay pass"),
       ]),
+      node(.2, 'generate new ideas and rank existing')
     ]),
-    node(.1, 'improvement', [
-      category(2400, 'study', sub_categories([69.5, 57, 50.5, 48, 6, 1])),
-      category(1500, 'debug'),
-      category(1200, 'meeting'),
-      category(1100, 'plan'),
-      category(900, 'errands', sub_categories([107, 15, 11.5, 4.5, 3.5])),
-      category(800, 'setup'),
-      category(800, 'coding'),
-      category(500, 'talk'),
-      category(400, 'exercise'),
-      category(300, 'writing'),
-      category(200, 'break'),
-      category(200, 'thinking'),
-      category(100, 'daydream'),
+    node(.2, 'improvement', [
+      node(.8, 'categorized_topics', [
+        category(2500, 'study', sub_categories([69.5, 57, 50.5, 48, 6, 1])),
+        category(1500, 'debug'),
+        category(1300, 'meeting'),
+        category(1200, 'plan'),
+        category(900, 'errands', sub_categories([107, 15, 11.5, 4.5, 3.5])),
+        category(900, 'setup'),
+        category(800, 'coding'),
+        category(500, 'talk'),
+        category(400, 'exercise'),
+        category(300, 'writing'),
+        category(200, 'break'),
+        category(200, 'thinking'),
+        category(100, 'daydream'),
+      ]),
+      node(.1, 'establish metrics'),
+      node(.1, 'review previous work'),
     ]),
-    node(.3, 'learning', [
-      node(.2, 'math'),
-      node(.2, 'cognition'),
-      node(.2, 'machine learning'),
-      node(.2, 'natural language processing'),
-      node(.2, 'compilers'),
+    node(.2, 'learning', [
+      node(.3, 'read list of books and articles', [
+        node(.9, 'read'),
+        node(.1, 'triage'),
+      ]),
+      node(.5, 'computers', [
+        node(.3, 'machine learning'),
+        node(.3, 'natural language processing'),
+        node(.2, 'compilers'),
+        node(.2, 'operating systems'),
+      ]),
+      node(.5, 'fundamentals', [
+        node(.4, 'math'),
+        node(.3, 'cognition'),
+        node(.1, 'biology'),
+        node(.1, 'chemistry'),
+        node(.1, 'physics'),
+      ]),
+      node(.1, 'read to-read articles'),
     ]),
     node(.2, 'networking'),
-    node(.1, 'wildcard'),
-    node(.1, 'review previous work'),
+    node(.033, 'errands'),
+    node(.033, 'finances'),
+    node(.033, 'wildcard'),
   ]
 
   print [node['name'] for node in weighted_choice(categories)]
